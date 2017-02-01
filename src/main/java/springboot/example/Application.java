ba275fb64c430273ceb6fe4a189ef4c982744651
stepsoft <1690036879@qq.com>
2017-02-01 11:22:18 +0800

    Changed message

--- src/main/java/springboot/example/Application.java
+++ src/main/java/springboot/example/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     String home() {
-        return "hello";
+        return "hello world";
     }
 
     public static void main(String[] args) {
